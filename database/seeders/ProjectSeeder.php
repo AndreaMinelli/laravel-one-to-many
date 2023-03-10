f25230cf47af59517cf6ab06eb5401986eab614d
AndreaMinelli <minelliandrea94@gmail.com>
2023-03-10 15:51:47 +0100

    Added null into type_id in ProjectSeeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -19,6 +19,7 @@ class ProjectSeeder extends Seeder
     public function run(Faker $faker): void
     {
         $type_id = Type::select('id')->pluck('id')->all();
+        $type_id[] = null;
 
         for ($i = 0; $i < 20; $i++) {
             $project = new Project();
