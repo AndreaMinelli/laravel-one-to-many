f100daa4504187ff734b18747ef824ad9c5e352e
AndreaMinelli <minelliandrea94@gmail.com>
2023-03-10 15:48:37 +0100

    Added type_id into ProjectSeeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -3,19 +3,26 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
+use Illuminate\Support\Facades\Facade;
 
 class ProjectSeeder extends Seeder
 {
+
+
     /**
      * Run the database seeds.
      */
     public function run(Faker $faker): void
     {
+        $type_id = Type::select('id')->pluck('id')->all();
+
         for ($i = 0; $i < 20; $i++) {
             $project = new Project();
+            $project->type_id = $faker->randomElement($type_id);
             $project->name = $faker->words(2, true);
             $project->description = $faker->paragraphs(4, true);
             $project->project_link = $faker->url();
