5baf7b52a1ddb605c5bde79ef8f7ee56fa77a173
AndreaMinelli <minelliandrea94@gmail.com>
2023-03-10 15:07:48 +0100

    Created TypeSeeder

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,12 @@ class TypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $types = ['Front-end', 'Back-end', 'Full-stack'];
+
+        foreach ($types as $type) {
+            $new_type = new Type();
+            $new_type->name = $type;
+            $new_type->save();
+        }
     }
 }
